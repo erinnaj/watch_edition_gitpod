f3156d76695621d87834ba36a83c6e70d3d2a74d
erinnaj <erinna.j@gmail.com>
2020-03-03 07:34:50 +0000

    [Bruce/Sheila][#0] Add names Bruce and Sheila to team page

--- client/src/js/pages/teamPage/teamPage.spec.js
+++ client/src/js/pages/teamPage/teamPage.spec.js
@@ -12,6 +12,12 @@ describe('TeamPage', () => {
       const page = new TeamPage();
       expect(page.render()).toContain("<h1>Made by:</h1>");
     });
+
+    it('should contain team members names', () => {
+      const page = new TeamPage();
+      expect(page.render()).toContain("Sheila");
+      expect(page.render()).toContain("Bruce");
+    });
   });
 
 });
